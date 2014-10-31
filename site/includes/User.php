097ed87305c0e9c844ce52ddf92760b349761385
William <willi.wambu@gmail.com>
2014-10-31 16:06:31 +0300

    Revert "Modified User class to take of user password."
    This reverts commit 9523add58b9f26bbd35fa4f3c845dd11609cd0fa.

--- site/includes/User.php
+++ site/includes/User.php
@@ -6,16 +6,14 @@ class User {
         $phone_number,
         $email_address,
         $profile_picture,
-        $password,
         $con;
 
-    function __construct($name,$location,$phone_number,$email_address,$profile_picture,$password){
+    function __construct($name,$location,$phone_number,$email_address,$profile_picture){
         $this -> name = $name;
         $this -> location = $location;
         $this -> phone_number = $phone_number;
         $this -> email_address = $email_address;
         $this -> profile_picture = $profile_picture;
-        $this -> password = $password;
 
         $this -> con = (new DbConnect()) -> getConnection();
     }
@@ -40,18 +38,10 @@ class User {
     public function getProfilePicture(){
         return $this -> profile_picture;
     }
-    public function getPassword(){
-        return $this -> password;
-    }
     public function insertUser(){
-        $query = "INSERT INTO tbl_users(name,location,phone_number,email_address,profile_picture,password)
-                  VALUES ('$this->name','$this->location','$this->phone_number','$this->email_address','$this->profile_picture','$this->password')";
-        if($this ->con-> query($query)){
-            return true;
-        }
-        else{
-            return false;
-        }
+        $query = "INSERT INTO tbl_users(name,location,phone_number,email_address,profile_picture)
+                  VALUES ('$this->name','$this->location','$this->phone_number','$this->email_address','$this->profile_picture')";
+        $this ->con-> query($query);
     }
    /**
      * get a user given the user id
@@ -66,7 +56,7 @@ class User {
 
         if($results){
             $array = $results -> fetch_all(MYSQL_ASSOC);
-            $user = new User($array['name'],$array['location'],$array['phone_number'],$array['email_address'],$array['profile_pictures'],$array['password']);
+            $user = new User($array['name'],$array['location'],$array['phone_number'],$array['email_address'],$array['profile_pictures']);
             $user ->setId($array['id']);
             return $user;
         }
@@ -78,9 +68,9 @@ class User {
      * update a user
      * @return true if successful and false otherwise
      */
-    public static function updateUser($id,$name,$location,$phone_number,$email_address,$profile_picture,$password){
+    public static function updateUser($id,$name,$location,$phone_number,$email_address,$profile_picture){
         $query = "UPDATE SET name='$name' location='$location' phone_number = '$phone_number' email_address='$email_address'
-                  profile_picture='$profile_picture' password='$password' WHERE id=$id";
+                  profile_picture='$profile_picture' WHERE id=$id";
         $connection =(new DbConnect()) -> getConnection();
         if ($connection->query($query)) {
             return true;
